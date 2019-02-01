b857aa1f448dc3a031cab7170813135b47644321
Alvarez Garcia <David.AlvarezGarcia@pacificorp.com>
2019-02-01 12:59:17 -0800

    adding installation of bashmarks

--- install.sh
+++ install.sh
@@ -18,3 +18,23 @@ ln -sfv "$DOTFILES_DIR/.vim/shortcuts.vim" ~/.vim/shortcuts.vim
 ln -sfv "$DOTFILES_DIR/.vim/colors.vim" ~/.vim/colors.vim
 ln -sfv "$DOTFILES_DIR/.vim/plugins.vim" ~/.vim/plugins.vim
 ln -sfv "$DOTFILES_DIR/.vim/config.vim" ~/.vim/config.vim
+
+##Adding pluggins###
+bash_apps_path=$HOME/dev/bash
+mkdir -p $bash_apps_path
+
+#Check make is installed
+make -h>/dev/null 2>&1
+make_installed=$?
+
+if [ -d "~/.local/bin/bashmarks.sh" ]; then
+	echo "Bashmarks already installed"
+elif hash make 2>/dev/null; then
+	cd $bash_apps_path
+	git clone https://github.com/vecin2/bashmarks.git
+	cd bashmarks
+	make install
+	source $HOME/.local/bin/bashmarks.sh
+else
+	echo "Bashmark cannot be installed because 'make' is not installed."
+fi
