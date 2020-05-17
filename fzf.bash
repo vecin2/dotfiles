a1558be14cb00ca9e7dfc007a136b16382325b0d
vecin2 <vecin2@gmail.com>
2020-05-17 15:08:52 -0400

    add binding to  open vim and to copy to clipboard

--- fzf.bash
+++ fzf.bash
@@ -3,13 +3,25 @@
 #export FZF_DEFAULT_OPTIONS="--extended --color fg:241,bg:230,hl:33,fg+:241,bg+:221,hl+:33 --color info:33,prompt:33,pointer:166,marker:166,spinner:33"
 # Setting fd as the default source for fzf
 #export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_DEFAULT_COMMAND='ag --path-to-ignore ~/.ignore --hidden -g "" --follow'
+export FZF_DEFAULT_COMMAND='ag --path-to-ignore ~/.ignore -g "" --follow'
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export MY_BASHRC_VAR="$FZF_DEFAULT_COMMAND"
 if [[ ! "$PATH" == */home/dgarcia/.fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/home/dgarcia/.fzf/bin"
 fi
+fzf-binds() {
+	printf 'ctrl-e:execute(echo {+} | xargs -o ${EDITOR:-vim})+abort,'
+        printf 'ctrl-y:execute-silent(echo {} | xargs echo -n | xclip -selection clipboard),'
+        printf 'ctrl-r:execute-silent(realpath {} | xargs echo -n | xclip -selection clipboard)'
+}
+
+#export FZF_DEFAULT_OPTS="--bind '$(fzf-binds)'"
+export FZF_DEFAULT_OPTS="
+        --bind 'ctrl-v:execute(less -K {})'
+	--bind 'ctrl-e:execute(vim {} < /dev/tty > /dev/tty)+abort'
+        --bind 'ctrl-y:execute-silent(echo {} | xargs echo -n | xclip -selection clipboard)'
+        --bind 'ctrl-r:execute-silent(realpath {} | xargs echo -n | xclip -selection clipboard)'"
 
 # Auto-completion
 # ---------------
@@ -38,22 +50,22 @@ fkill() {
 #   - CTRL-N or Enter key to navigate in natilus
 
 
-fo() {
-	local out file key
-	IFS=$'\n'
- 	out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e,ctrl-n)
-	key=$(head -1 <<< "$out")
-	file=$(head -2 <<< "$out" | tail -1)
-	if [ -n "$file" ]; then
-	 if [ "$key" = ctrl-e ]; then
-		 ${EDITOR:-vim} "$file"
-	 elif [ "$key" = ctrl-n ]; then
-		 nautilus "$file"
-	 else
-		 xdg-open "$file"
-	 fi
-	fi
-}
+#fo() {
+#	local out file key
+#	IFS=$'\n'
+# 	out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e,ctrl-n)
+#	key=$(head -1 <<< "$out")
+#	file=$(head -2 <<< "$out" | tail -1)
+#	if [ -n "$file" ]; then
+#	 if [ "$key" = ctrl-e ]; then
+#		 ${EDITOR:-vim} "$file"
+#	 elif [ "$key" = ctrl-n ]; then
+#		 nautilus "$file"
+#	 else
+#		 xdg-open "$file"
+#	 fi
+#	fi
+#}
 #bind '"\C-o":"fo\n"'
 
 
